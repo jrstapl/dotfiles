6fa3f80d06df994507659b9804614f294a4fa1b2
jrstaple <joshrstapleton@gmail.com>
2026-01-19 12:45:59 -0500

    add pyright with disabled formatting and stylua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,12 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-
-require("config.lazy")
+require "config.lazy"
 
 vim.opt.shiftwidth = 4
 -- vim.opt.clipboard = "unnamedplus" -- for using system clip with P, otherwise use <C-V> for system and p/P for vim buffer
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
@@ -18,52 +16,52 @@ vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-	callback = function()
-		vim.opt.number = false
-		vim.opt.relativenumber = false
-	end,
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
 })
 
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		if vim.fn.argc() == 0 then
-			vim.schedule(function()
-				pcall(Snacks.dashboard())
-			end)
-		end
-	end,
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.schedule(function()
+        pcall(Snacks.dashboard())
+      end)
+    end
+  end,
 })
 
 local job_id = 0
 vim.keymap.set("n", "<space>to", function()
-	vim.cmd.vnew()
-	vim.cmd.term()
-	vim.cmd.wincmd("J")
-	vim.api.nvim_win_set_height(0, 5)
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd "J"
+  vim.api.nvim_win_set_height(0, 5)
 
-	job_id = vim.bo.channel
+  job_id = vim.bo.channel
 end)
 
 local current_command = ""
 vim.keymap.set("n", "<space>te", function()
-	current_command = vim.fn.input("Command: ")
+  current_command = vim.fn.input "Command: "
 end)
 
 vim.keymap.set("n", "<space>tr", function()
-	if current_command == "" then
-		current_command = vim.fn.input("Command: ")
-	end
+  if current_command == "" then
+    current_command = vim.fn.input "Command: "
+  end
 
-	vim.fn.chansend(job_id, { current_command .. "\r\n" })
+  vim.fn.chansend(job_id, { current_command .. "\r\n" })
 end)
 
 vim.keymap.set("n", "-", "<cmd>Oil<CR>")
@@ -72,30 +70,30 @@ vim.keymap.set("n", "-", "<cmd>Oil<CR>")
 vim.opt.termguicolors = true
 require("bufferline").setup {}
 
-
 -- conform
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "ruff_format" },
-		go = { "goimports", "gofmt" }
-	},
-	format_on_save = {
-		lsp_format = "fallback",
-		timeout_ms = 500,
-	}
-})
+require("conform").setup {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = { "ruff_format" },
+    go = { "goimports", "gofmt" },
+  },
+  format_on_save = {
+    lsp_format = "fallback",
+    timeout_ms = 500,
+  },
+}
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
 })
 
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("gopls")
-vim.lsp.enable("ruff")
-vim.lsp.enable("ols")
-vim.lsp.enable("clangd")
-vim.lsp.enable("cmake")
+vim.lsp.enable "lua_ls"
+vim.lsp.enable "gopls"
+vim.lsp.enable "ruff"
+vim.lsp.enable "ols"
+vim.lsp.enable "clangd"
+vim.lsp.enable "cmake"
+vim.lsp.enable "pyright"
