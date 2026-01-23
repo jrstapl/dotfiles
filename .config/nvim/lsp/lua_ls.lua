bc092c557649dd1e5edadb26dcde01802112d8e3
jrstaple <joshrstapleton@gmail.com>
2026-01-23 15:11:11 -0500

    add vim runtime to lua

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -1,21 +1,27 @@
 return {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
   root_markers = {
-    '.emmyrc.json',
-    '.luarc.json',
-    '.luarc.jsonc',
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
-    '.git',
+    ".emmyrc.json",
+    ".luarc.json",
+    ".luarc.jsonc",
+    ".luacheckrc",
+    ".stylua.toml",
+    "stylua.toml",
+    "selene.toml",
+    "selene.yml",
+    ".git",
   },
   settings = {
     Lua = {
       codeLens = { enable = true },
-      hint = { enable = true, semicolon = 'Disable' },
+      hint = { enable = true, semicolon = "Disable" },
+      workspace = {
+        checkThirdParty = true,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
     },
   },
 }
