9faf6e56dc09e77cf6f9618de38d3a610e67e9b7
jrstaple <joshrstapleton@gmail.com>
2026-03-06 08:59:09 -0500

    update telescope leader commands

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -24,7 +24,10 @@ return {
       }
 
       require("telescope").load_extension "fzf"
-
+      local builtin = require "telescope.builtin"
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
       vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
       vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
       vim.keymap.set("n", "<space>en", function()
